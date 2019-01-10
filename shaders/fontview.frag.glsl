4441462b20cc109b1238b3f1772d164a5ae608d5
LambdaXymox <lambda.xymox@gmail.com>
2019-01-09 18:45:07 -0600

    Fix spelling in fragment shader.

--- shaders/fontview.frag.glsl
+++ shaders/fontview.frag.glsl
@@ -2,10 +2,10 @@
 
 in vec2 st;
 uniform sampler2D tex;
-uniform vec4 text_colour;
-out vec4 frag_colour;
+uniform vec4 text_color;
+out vec4 frag_color;
 
 
 void main () {
-    frag_colour = texture (tex, st) * text_colour;
+    frag_color = texture (tex, st) * text_color;
 }
